c04ac7feae3998f7f8b1919a6657d2812ddb7848
Sean <sean.hong@linecorp.com>
2025-03-06 22:48:28 +0800

    chore: update mapping

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -48,7 +48,7 @@ lspconfig.vimls.setup({})
 lspconfig.lua_ls.setup({
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       return
     end
 
@@ -134,12 +134,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
     --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
     --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
     --vim.keymap.set('n', '<space>wl', function()
-      --print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    --print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     --end, opts)
     --vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<A-r>', vim.lsp.buf.rename, opts)
