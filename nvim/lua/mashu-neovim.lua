7d9bc74c4f5c0ab8ea8b833920444fb95fdd9041
Sean <sean.hong@linecorp.com>
2025-04-30 02:08:33 +0800

    chore: update lazy-lock

--- nvim/lua/mashu-neovim.lua
+++ nvim/lua/mashu-neovim.lua
@@ -134,11 +134,11 @@ end
 
 local function set_up_neovide()
   vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
-  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
-  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
-  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
-  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
-  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+  vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
   vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
 
