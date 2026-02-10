1f7eaa1e8d7ec56f205f7bd00e80ff661b9626b3
Mashu <ndsl6211@gmail.com>
2026-02-10 10:12:39 +0800

    chore(nvim): add schemastore and update jsonls config

--- nvim/lsp/jsonls.lua
+++ nvim/lsp/jsonls.lua
@@ -1,3 +1,9 @@
 return {
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  settings = {
+    json = {
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
+    }
+  }
 }
